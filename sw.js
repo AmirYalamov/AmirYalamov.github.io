6ada7d94ff8f4a7bc4e5bcdb786e2730bb19a7ae
Amir Yalamov <amir.yalamov@gmail.com>
2020-05-26 17:37:43 -0400

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-f0bc41c23099695e25a7.js"
+    "url": "webpack-runtime-555645f2a5f2a6045563.js"
   },
   {
     "url": "framework-8770a85f31e633ae9ae0.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "0e7299100d688ba3107d98192626375d"
+    "revision": "8c4642e91a887829603234efa36c7b11"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-392971c0bbe245cc91a1.js"
